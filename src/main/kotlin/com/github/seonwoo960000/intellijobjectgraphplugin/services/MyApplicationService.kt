0f4975beee14ab7861dcfb052d77822d3ef07d02
GitHub Action <action@github.com>
2022-11-18 07:22:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/seonwoo960000/intellijobjectgraphplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/seonwoo960000/intellijobjectgraphplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.seonwoo960000.intellijobjectgraphplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.seonwoo960000.intellijobjectgraphplugin.MyBundle
 
 class MyApplicationService {
 
